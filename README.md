64a26b30fcf3be7fc5f814690dd8263f96671e8e
Braden Clevenger <bradenclevenger5@icloud.com>
2023-07-17 19:36:26 -0400

    commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 03 
+# 03 password-generator 
 
 ## Your Task
 
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,7 @@
 // Assignment Code
-var form = document.querySelector('#passwordGeneratorForm');
-var characterAmountNumber = document.querySelector('#characterAmountNumber');
-var includeUppercase = document.querySelector('#includeUppercase');
-var includeNumbers = document.querySelector('#includeNumbers');
-var includeSymbols = document.querySelector('#includeSymbols');
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -14,14 +10,60 @@ function writePassword() {
 
 }
 
-function generatePassword () {
-  let lowercase =
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 generateBtn.addEventListener("click", writePassword);
 
-form.addEventListener("submit" , e => {
-    e.preventDefault()
-    const characterAmount = characterAmountNumber.value
-    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-})
+function generatePassword() {
+  while (true) {
+  var length = prompt ("Enter the desired length of your password between 8 and 128 characters):");
+  length = parseInt(length); 
+
+  if (!isNaN(length) && length >= 8 && length <= 128) {
+    break;
+  }
+
+  alert ("invalid length! Please enter a number between 8 and 128");
+  }
+
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeNumbers = confirm("Include numbers");
+  var includeSpecialChars = confirm("Include special characters?");
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+=[]{}|\;:<>,.?/";
+
+
+  var selectedChars = "";
+
+  if (includeLowercase) {
+    selectedChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    selectedChars += uppercaseChars;
+  }
+  if (includeNumbers) {
+    selectedChars += numberChars;
+  }
+  if (includeSpecialChars) {
+    selectedChars += specialChars;
+  }
+
+  if (selectedChars === "") {
+    alert("You must select at least one character set.");
+    return "";
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * selectedChars.length);
+    password += selectedChars.charAt(randomIndex);
+  }
+
+  return password;
+
+}
